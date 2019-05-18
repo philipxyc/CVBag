ae05662767954a02f13b5caddac74608b6752c75
geekpie <geekpie@a.b.c>
2019-05-18 17:44:15 +0800

    obstacle detector

--- ObstacleDetector.py
+++ ObstacleDetector.py
@@ -67,42 +67,88 @@ if __name__ == "__main__":
 	config = rs.config()
 	config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
 	config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+
 	# Start streaming
 	pipeline.start(config)
 
-
-	color_contours = (0, 255, 0)
-	color_hull = (0, 0, 255)
 	try:
+		color_contours = (0, 255, 0)
+		color_hull = (0, 0, 255)
 		while True:
+
 			# Wait for a coherent pair of frames: depth and color
 			frames = pipeline.wait_for_frames()
 			depth_frame = frames.get_depth_frame()
 			color_frame = frames.get_color_frame()
 			if not depth_frame or not color_frame:
 				continue
+
+			# Convert images to numpy arrays
 			depth_image = np.asanyarray(depth_frame.get_data())
-			rgb_image = np.asanyarray(color_frame.get_data())
+			color_image = np.asanyarray(color_frame.get_data())
 
+			# Apply colormap on depth image (image must be converted to 8-bit per pixel first)
 			depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
-			# print(depth_colormap)
-			cv2.imshow('Webcam', depth_colormap)
-
 			# Detection
-			# hulls, contours, hierarchy = findObstacle(depth_image)
-			# for i in range(len(hulls)):
-			# 	# draw ith contour
-			# 	cv2.drawContours(depth_image, contours, i, color_contours, 1, 8, hierarchy)
-			# 	cv2.drawContours(depth_image, hulls, i, color_hull, 1, 8)
-			
-			# time.sleep(10)
+			hulls, contours, hierarchy = findObstacle(depth_image)
+			for i in range(len(hulls)):
+			 	# draw ith contour
+				cv2.drawContours(depth_colormap, contours, i, color_contours, 1, 8, hierarchy)
+				cv2.drawContours(depth_colormap, hulls, i, color_hull, 1, 8)
+			# Stack both images horizontally
+			images = np.hstack((color_image, depth_colormap))
 
-			# key = cv2.waitKey(1)
-			# Press esc or 'q' to close the image window
-			# if key & 0xFF == ord('q') or key == 27:
-			# 	cv2.destroyAllWindows()
-			# 	break
-			# break
+			# Show images
+			cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
+			cv2.imshow('RealSense', images)
+			cv2.waitKey(1)
+
 	finally:
+
 		# Stop streaming
 		pipeline.stop()
+
+	# # Configure depth and color streams
+	# pipeline = rs.pipeline()
+	# config = rs.config()
+	# config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+	# config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+	# # Start streaming
+	# pipeline.start(config)
+
+
+	# color_contours = (0, 255, 0)
+	# color_hull = (0, 0, 255)
+	# try:
+	# 	while True:
+	# 		# Wait for a coherent pair of frames: depth and color
+	# 		frames = pipeline.wait_for_frames()
+	# 		depth_frame = frames.get_depth_frame()
+	# 		color_frame = frames.get_color_frame()
+	# 		if not depth_frame or not color_frame:
+	# 			continue
+	# 		depth_image = np.asanyarray(depth_frame.get_data())
+	# 		rgb_image = np.asanyarray(color_frame.get_data())
+
+	# 		depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+	# 		# print(depth_colormap)
+	# 		cv2.imshow('Webcam', depth_colormap)
+
+	# 		# Detection
+	# 		# hulls, contours, hierarchy = findObstacle(depth_image)
+	# 		# for i in range(len(hulls)):
+	# 		# 	# draw ith contour
+	# 		# 	cv2.drawContours(depth_image, contours, i, color_contours, 1, 8, hierarchy)
+	# 		# 	cv2.drawContours(depth_image, hulls, i, color_hull, 1, 8)
+			
+	# 		# time.sleep(10)
+
+	# 		# key = cv2.waitKey(1)
+	# 		# Press esc or 'q' to close the image window
+	# 		# if key & 0xFF == ord('q') or key == 27:
+	# 		# 	cv2.destroyAllWindows()
+	# 		# 	break
+	# 		# break
+	# finally:
+	# 	# Stop streaming
+	# 	pipeline.stop()
