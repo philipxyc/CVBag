1236f2086d506665247b19abc692aae7077d3a9f
peihao wang <wangpeihao@gmail.com>
2019-05-23 17:57:06 +0800

    restore to Sphinx

--- voice.py
+++ voice.py
@@ -11,7 +11,6 @@ with sr.Microphone() as source:
    
 print("Now recognizing ...")
 
-'''
 # recognize speech using Sphinx  
 try:  
 	print("Sphinx thinks you said '" + r.recognize_sphinx(audio) + "'")  
@@ -19,13 +18,12 @@ except sr.UnknownValueError:
 	print("Sphinx could not understand audio")  
 except sr.RequestError as e:  
 	print("Sphinx error; {0}".format(e))
-'''
 
 # recognize speech using Microsoft Bing Voice Recognition
-AZURE_SPEECH_KEY = "b1b54e5bcd8943f0b8106e000e1298d7"  # Microsoft Speech API keys 32-character lowercase hexadecimal strings
-try:
-    print("Microsoft Azure Speech thinks you said " + r.recognize_azure(audio, key=AZURE_SPEECH_KEY))
-except sr.UnknownValueError:
-    print("Microsoft Azure Speech could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Microsoft Azure Speech service; {0}".format(e))
+# AZURE_SPEECH_KEY = "b1b54e5bcd8943f0b8106e000e1298d7"  # Microsoft Speech API keys 32-character lowercase hexadecimal strings
+# try:
+#     print("Microsoft Azure Speech thinks you said " + r.recognize_azure(audio, key=AZURE_SPEECH_KEY))
+# except sr.UnknownValueError:
+#     print("Microsoft Azure Speech could not understand audio")
+# except sr.RequestError as e:
+#     print("Could not request results from Microsoft Azure Speech service; {0}".format(e))
