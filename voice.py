fe353903af9693ae3fc8a11f741c6c22b0168a3e
peihao wang <wangpeihao@gmail.com>
2019-05-23 17:33:15 +0800

    azure voice

--- voice.py
+++ voice.py
@@ -1,6 +1,6 @@
 import speech_recognition as sr  
    
- # obtain audio from the microphone  
+# obtain audio from the microphone  
 r = sr.Recognizer()  
 with sr.Microphone() as source:  
 	print("Please wait. Calibrating microphone...")  
@@ -9,10 +9,23 @@ with sr.Microphone() as source:
 	print("Say something!")  
 	audio = r.listen(source)  
    
- # recognize speech using Sphinx  
+print("Now recognizing ...")
+
+'''
+# recognize speech using Sphinx  
 try:  
 	print("Sphinx thinks you said '" + r.recognize_sphinx(audio) + "'")  
 except sr.UnknownValueError:  
 	print("Sphinx could not understand audio")  
 except sr.RequestError as e:  
 	print("Sphinx error; {0}".format(e))
+'''
+
+# recognize speech using Microsoft Bing Voice Recognition
+BING_KEY = "b1b54e5bcd8943f0b8106e000e1298d7"  # Microsoft Bing Voice Recognition API keys 32-character lowercase hexadecimal strings
+try:
+    print("Microsoft Bing Voice Recognition thinks you said " + r.recognize_bing(audio, key=BING_KEY))
+except sr.UnknownValueError:
+    print("Microsoft Bing Voice Recognition could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from Microsoft Bing Voice Recognition service; {0}".format(e))
